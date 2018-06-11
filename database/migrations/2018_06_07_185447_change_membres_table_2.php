4f64d755c4379c2d3e1d9b9f3508a6fefc964f7a
Nicolas Ramos <contact@nicolas-r.fr>
2018-06-11 21:57:37 +0200

    Update and rename 2018_06_07_185447_change_membres_table.php to 2018_06_07_185447_change_membres_table_2.php

--- database/migrations/2018_06_07_185447_change_membres_table_2.php
+++ database/migrations/2018_06_07_185447_change_membres_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeMembresTable extends Migration
+class ChangeMembres2Table extends Migration
 {
     /**
      * Run the migrations.
