2070c2eb1faeb1e0b73c0968b7d9229d220e17c3
Mortim <33499834+mortim59@users.noreply.github.com>
2018-06-12 18:26:11 +0200

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,21 @@
 <header id="fond_menu">
 
-	<h1 class="title-website"><a href="/accueil" style="text-decoration: none;color: #fff;">TheGreatComputing</a></h1>
+	<h1 class="title-website"><a href="/" style="text-decoration: none;color: #fff;">TheGreatComputing</a></h1>
 	<nav id="menu">
 		<ul>
 			@if(auth()->check())
-				<li><a href="../deconnexion"><i class="fas fa-sign-out-alt"></i> DECONNEXION</a></li>
+				<li><a href="/deconnexion"><i class="fas fa-sign-out-alt"></i> DECONNEXION</a></li>
 			@if(Auth::user()['is_redactor'] OR Auth::user()['is_admin'])
 				<li><a href="{{ route('articles.create') }}"><i class="fas fa-pencil-alt"></i> CREER UN ARTICLE</a></li>
 			@endif
 				<li><a href="/profile/{{ Auth::user()->pseudo }}"><span class="glyphicon glyphicon-user"></span> MON COMPTE</a></li>
 			@endif
-				<li><a href="../articles">TOUS LES ARTICLES</a></li>
-			<li><a href="../inscription">INSCRIPTION</a></li>
+				<li><a href="/articles">TOUS LES ARTICLES</a></li>
+			<li><a href="/inscription">INSCRIPTION</a></li>
 			@if(!auth()->check())
-				<li><a href="../connexion">CONNEXION</a></li>
+				<li><a href="/connexion">CONNEXION</a></li>
 			@endif
-			<li><a href="/accueil"><i class="fas fa-home"></i> ACCUEIL</a></li>
+			<li><a href="/"><i class="fas fa-home"></i> ACCUEIL</a></li>
 		</ul>
 	</nav>
 
